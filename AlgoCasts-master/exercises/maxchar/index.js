f5d95cfdf7d1372341a07c5a20a48cfb1ad55b47
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-01-23 22:52:59 +0900

    [feat] 2 codes

--- AlgoCasts-master/exercises/maxchar/index.js
+++ AlgoCasts-master/exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const array = str.split("");
+  let maxValue = 0;
+  let maxChar = "";
+  const charsObj = {};
+
+  for (let element of array) {
+    if (!charsObj[element]) {
+      charsObj[element] = 1;
+    } else {
+      charsObj[element]++;
+    }
+  }
+  const keyArr = Object.keys(charsObj);
+  keyArr.forEach((key) => {
+    if (charsObj[key] > maxValue) {
+      maxValue = charsObj[key];
+      maxChar = key;
+    }
+  });
+  console.log(maxChar);
+  return maxChar;
+}
 
 module.exports = maxChar;
--- AlgoCasts-master/exercises/reverseint/index.js
+++ AlgoCasts-master/exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const sign = Math.sign(n);
+  const array = String(Math.abs(n))
+    .split("")
+    .reduce((acc, currentValue) => {
+      return currentValue + acc;
+    }, "");
+  return sign * Number(array);
+}
 
 module.exports = reverseInt;
